f9a78831c105ae92d702be39190d79a4dcdf4572
Cicilini <guilherme.cicilini@hotmail.com>
2024-01-26 23:21:09 -0300

    abrir e fechar modal

--- pages/tela-usuário-1/script.js
+++ pages/tela-usuário-1/script.js
@@ -1,1 +1,10 @@
+let modal = document.getElementsByClassName("container-modal")[0]
 
+
+function openModal() {
+    modal.style.display = "flex";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
