603254b296e1fc2650921cc91da995327f13f9b1
Cicilini <guilherme.cicilini@hotmail.com>
2024-01-29 23:37:06 -0300

    finalizacao do segundo modal + interatividade

--- pages/tela-usuário-1/script.js
+++ pages/tela-usuário-1/script.js
@@ -1,10 +1,19 @@
-const modal = document.getElementsByClassName("container-modal")[0]
-const BtnInput = document.getElementsByClassName("enviar-projeto-2")[0]
-const inputIMG = document.getElementById("input-file")
-let preview = document.getElementById("exibir-projeto")
+const modal = document.getElementsByClassName("container-modal")[0] // Primeiro modal
+const modal2 = document.getElementsByClassName("modal-2")[0] // Segundo modal
 
-console.log(preview)
+const BtnInput = document.getElementsByClassName("enviar-projeto-2")[0] // Btn adicionar imagem no primeiro modal
+const inputIMG = document.getElementById("input-file") // input invisivel associado ao form e button acima
 
+const containerModais = document.getElementsByClassName("container-modal")[0] //container que envolve os modais
+let preview = document.getElementById("exibir-projeto") //Imagem a ser exibida no primeiro modal
+let preview2 = document.getElementById("exibir-projeto-2") //Imagem a ser exibida no segundo modal
+
+// Detalhe: Como há dois locais onde as imagens carregas forão exibidas, com medidas diferentes
+// optei por utilizar um id para cada
+
+
+
+/*Primeiro modal / abrir e fechar*/
 function openModal() {
     modal.style.display = "flex";
 }
@@ -15,13 +24,30 @@ function closeModal() {
     BtnInput.style.display = 'inline';
 }
 
+
+/*Segundo modal / abrir e fechar
+
+Detalhe: Precisei mexer na altura do container que envolve o modal (sombra) devido ao bug de altura  */
+function openModal2(){
+    modal2.style.display = "flex";
+    containerModais.style.height = "75rem"
+}
+
+function closeModal2(){
+    modal2.style.display = "none";
+    containerModais.style.height = "57rem"
+}
+
+
+
+//Criei essa funcao associada ao botao para que ela chame o input (que está invisível)
 BtnInput.addEventListener("click", () => {
     inputIMG.click()
 })
 
-
+// Funcionalidade para o usuário enviar a imagem e ela s
 inputIMG.addEventListener("change", () => {
-    var file = inputIMG.files[0]
+    let file = inputIMG.files[0]
 
     if (file) {
         var leitura = new FileReader();
@@ -29,6 +55,7 @@ inputIMG.addEventListener("change", () => {
             var imageUrl = event.target.result
 
             preview.src = imageUrl;
+            preview2.src= imageUrl;
             preview.style.display = 'inline';
             BtnInput.style.display = 'none';
         };
