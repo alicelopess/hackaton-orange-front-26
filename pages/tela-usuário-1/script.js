e88576b8caa5527a7d9a0d748ebbe1b17076e104
Cicilini <guilherme.cicilini@hotmail.com>
2024-01-31 23:29:02 -0300

    preview finalizado

--- pages/tela-usuário-1/script.js
+++ pages/tela-usuário-1/script.js
@@ -1,33 +1,36 @@
-const modal = document.getElementsByClassName("container-modal")[0] // Container envolvendo os modais
-const modal1 = document.getElementsByClassName("modal-1")[0] // Primeiro modal
-const modal2 = document.getElementsByClassName("modal-2")[0] // Segundo modal
-const modal3 = document.getElementsByClassName("modal-3")[0] //último modal
+// Selecionando elementos do DOM
+const modal = document.getElementsByClassName("container-modal")[0]; // Container envolvendo os modais
+const modal1 = document.getElementsByClassName("modal-1")[0]; // Primeiro modal
+const modal2 = document.getElementsByClassName("modal-2")[0]; // Segundo modal
+const modal3 = document.getElementsByClassName("modal-3")[0]; // Último modal
 
-const BtnInput = document.getElementsByClassName("enviar-projeto-2")[0] // Btn adicionar imagem no primeiro modal
-const inputIMG = document.getElementById("input-file") // input invisivel associado ao form e button acima
+const BtnInput = document.getElementsByClassName("enviar-projeto-2")[0]; // Botão para adicionar imagem no primeiro modal
+const inputIMG = document.getElementById("input-file"); // Input invisível associado ao formulário e botão acima
 
+// Inputs do formulário
+let inptTitle = document.getElementById("titulo");
+let inptLink = document.getElementById("link");
+let inptDescricao = document.getElementById("descricao");
+let inptTag = document.getElementById("tags");
 
+// Campos da prévia onde os inputs serão exibidos
+let previewTitle = document.querySelector(".title h5");
+let previewDescri = document.querySelector("#des-1");
+let previewLink = document.querySelector(".descricao a");
 
+console.log(previewDescri)
+console.log(previewLink)
 
+// Imagens a serem exibidas nos modais
+const containerModais = document.getElementsByClassName("container-modal")[0]; // Container que envolve os modais
+let preview = document.getElementById("exibir-projeto"); // Imagem exibida no primeiro modal
+let preview2 = document.getElementById("exibir-projeto-2"); // Imagem exibida no segundo modal
 
+// Inputs/Submissões do formulário e botões associados
+const inpSubmit = document.getElementById("Enviar"); // Input/Submit do formulário (Enviar)
+const btnSalvar = document.getElementById("btnSalvar"); // Botão para enviar o formulário
 
-//  Detalhe: Como há dois locais onde as imagens são exibidas, com medidas diferentes
-//  optei por utilizar um id para cada
-
-const containerModais = document.getElementsByClassName("container-modal")[0] //container que envolve os modais
-let preview = document.getElementById("exibir-projeto") //Imagem a ser exibida no primeiro modal
-let preview2 = document.getElementById("exibir-projeto-2") //Imagem a ser exibida no segundo modal
-
-
-// Por algum motivo, não consegui utilizar o submit fora do form no HTML e manter o container do modal aberto,
-// então optei por criar um outro botão e integrar os dois..
-
-const inpSubmit = document.getElementById("Enviar") // Input/Submir do formulário (Enviar)
-const btnSalvar = document.getElementById("btnSalvar") //Botão para enviar o formulário
-
-
-
-/*Primeiro modal / abrir e fechar*/
+// Abre e fecha o primeiro modal
 function openModal() {
     modal.style.display = "flex";
 }
@@ -36,37 +39,34 @@ function closeModal() {
     modal.style.display = "none";
     preview.style.display = 'none';
     BtnInput.style.display = 'inline';
-    preview2.src = "/"
+    preview2.src = "/";
+    
+    LimparForm()
 }
 
-
-/*Segundo modal / abrir e fechar
-Detalhe: Precisei mexer na altura do container que envolve o modal (sombra) devido ao bug de altura  */
+// Abre e fecha o segundo modal
 function openModal2() {
     modal2.style.display = "flex";
-    containerModais.style.height = "75rem"
-
-    autocompletar()
+    containerModais.style.height = "75rem";
+    autocompletar();
 }
 
 function closeModal2() {
     modal2.style.display = "none";
-    containerModais.style.height = "57rem"
+    containerModais.style.height = "57rem";
 }
 
-
-/* Útimo modal / abrir e fechar */
+// Abre e fecha o último modal
 function openModal3() {
     modal.style.display = "flex";
     modal1.style.display = "none";
     modal3.style.display = "flex";
 
-    //reset nos campos das imagens carregadas
-    preview2.src = "/"
+    // Reset nos campos das imagens carregadas
+    preview2.src = "/";
     preview.style.display = 'none';
     BtnInput.style.display = 'inline';
-
-
+    LimparForm()
 }
 
 function closeModal3() {
@@ -75,21 +75,19 @@ function closeModal3() {
     modal1.style.display = "inline";
 }
 
-
-
-//Criei essa funcao associada ao botao para que ela chame o input (que está invisível)
+// Função associada ao botão para abrir o input de arquivo invisível
 BtnInput.addEventListener("click", () => {
-    inputIMG.click()
-})
+    inputIMG.click();
+});
 
-// Funcionalidade para o usuário enviar a imagem e ela s
+// Funcionalidade para o usuário enviar a imagem
 inputIMG.addEventListener("change", () => {
-    let file = inputIMG.files[0]
+    let file = inputIMG.files[0];
 
     if (file) {
         var leitura = new FileReader();
         leitura.onload = function (event) {
-            var imageUrl = event.target.result
+            var imageUrl = event.target.result;
 
             preview.src = imageUrl;
             preview2.src = imageUrl;
@@ -98,44 +96,37 @@ inputIMG.addEventListener("change", () => {
         };
         leitura.readAsDataURL(file);
     } else {
-        alert('Nenhum arquivo selecionado.')
+        alert('Nenhum arquivo selecionado.');
     }
 });
 
-
-
-
- //Em desenvolvimento
-//Criei essa função para integrar o que está sendo preenchido nos campos do formulário
-//com o que está sendo exibido no segundo modal
-//Detalhe: adiconei a chamada dessa função dentro da openModal2
-
+// Preenche os campos da prévia com o que está sendo preenchido nos campos do formulário
+// Funcao sendo chamada dentro do openModal2(botao de visualzação)
 function autocompletar() {
-    //inputs do form
-    let inptTitle = document.getElementById("titulo")
-    let inptLink = document.getElementById("link")
-    let inptDescricao = document.getElementById("descricao")
-
-    //Campos da prévia onde eu vou exibir o que há no input
-    let previewTitle = document.querySelector(".title h5")
-    let previewDescri = document.querySelector("#des-1")
-    let previewLink = document.querySelector(".descricao a")
-
-
-    if(inptTitle.value === ""){
-        previewTitle.textContent = "(Título do projeto)"
+    if (inptTitle.value === "") {
+        previewTitle.textContent = "Título do projeto";
     } else {
-        previewTitle.textContent = inptTitle.value
+        previewTitle.textContent = inptTitle.value;
     }
 
-    previewTitle.textContent = inptTitle.value;
-    previewDescri.textContent = inptDescricao.value;
-    previewLink.textContent = inptTitle.value;   
+    if (inptDescricao.value === "") {
+        previewDescri.textContent = "(Descrição)";
+    } else {
+        previewDescri.textContent = inptDescricao.value;
+    }
+
+    if (inptLink.value === "") {
+        previewLink.textContent = "Link do projeto";
+    } else {
+        previewLink.textContent = inptLink.value;
+    }
 }
 
 
-
-
-
-
-
+//limpar campos do formulário
+function LimparForm() {
+    inptTitle.value = ""; 
+    inptLink.value = ""; 
+    inptDescricao.value = ""; 
+    inptTag.value = "";
+}
